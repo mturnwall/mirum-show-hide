e6918e93ee267af404440ce121ee4512e6611bd9
Mitchell Valine <mitchell.valine@mirumagency.com>
2017-03-16 16:26:12 -0700

    merge conflicts :

---
+++ test/show-hide.spec.js
@@ -0,0 +1,61 @@
+import test from 'ava';
+import expand, {getActualHeight, toggleHeight} from '../src/show_hide';
+import {stub} from 'sinon';
+
+const cloneNode = stub()
+const appendChild = stub()
+const removeChild = stub()
+const mockExtra = {
+    style: {
+        height: 'HEIGHT'
+    },
+    offsetHeight: 'OFFSET_HEIGHT',
+    parentNode: {
+        appendChild,
+        removeChild
+    }
+}
+const mockClone = Object.assign({}, mockExtra)
+mockExtra.cloneNode = cloneNode.returns(mockExtra)
+mockClone.cloneNode = mockExtra.cloneNode
+
+test.afterEach.always(() => {
+    cloneNode.reset()
+    appendChild.reset()
+    removeChild.reset()
+})
+
+test('getActualheight', t => {
+    const subject = getActualHeight(mockExtra)
+    t.true(
+        cloneNode.calledOnce && cloneNode.calledWithExactly(true),
+        'calls cloneNode with true'
+    )
+
+    t.true(
+        mockClone.style.height === 'auto',
+        'sets clone height to auto'
+    )
+
+    t.true(appendChild.calledOnce)
+
+    t.deepEqual(
+        appendChild.lastCall.args[0],
+        mockClone,
+        'appends clone to parentNode with height set to auto'
+    )
+
+    t.true(removeChild.calledOnce)
+
+    t.deepEqual(
+        removeChild.lastCall.args[0],
+        mockClone,
+        'removes clone from parentNode'
+    )
+
+    t.is(
+        subject,
+        mockExtra.offsetHeight,
+        'returns offset height of clone appended to parent node'
+    )
+})
